4825e3ee54cf3535cf91c38b45c7b985f92b552a
RomanKamlykov <romankamlikov@gmail.com>
2018-10-30 00:55:57 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_app_1_demo/precache-manifest.e2b0679a369a663c8954a296eb35a19b.js"
+  "/react_app_1/precache-manifest.9cb3aac63f471cd61345384165e50147.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react_app_1_demo/index.html", {
+workbox.routing.registerNavigationRoute("/react_app_1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
