bddc2793186d20aef4184a9a6d8e11208622845a
RomanKamlykov <romankamlikov@gmail.com>
2020-04-05 19:04:38 +0300

    Updates

---
+++ precache-manifest.2e3a52a2a5a426525e5dae51a4cef671.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7a0a55a74f632d21b964e0dd4004c7",
+    "url": "/react_app_1/index.html"
+  },
+  {
+    "revision": "b6c48466481d293b550e",
+    "url": "/react_app_1/static/css/main.2fb06351.chunk.css"
+  },
+  {
+    "revision": "2825044b37fcb9cd9d1d",
+    "url": "/react_app_1/static/js/2.23e67286.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app_1/static/js/2.23e67286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c48466481d293b550e",
+    "url": "/react_app_1/static/js/main.205dea59.chunk.js"
+  },
+  {
+    "revision": "69fef59824eb700f40cc",
+    "url": "/react_app_1/static/js/runtime-main.2621fbee.js"
+  },
+  {
+    "revision": "d42baa5988ff730bb4c4569508b20eb1",
+    "url": "/react_app_1/static/media/ComicRunes.d42baa59.otf"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/react_app_1/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "49f89e34d03233b1f27788f75df7a40a",
+    "url": "/react_app_1/static/media/OpenSans-ExtraBold.49f89e34.ttf"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/react_app_1/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/react_app_1/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "479b3752429ca8a4995014f4e13b5676",
+    "url": "/react_app_1/static/media/background2.479b3752.png"
+  },
+  {
+    "revision": "51221521bc9897de230a7940f66f9564",
+    "url": "/react_app_1/static/media/ellipse_big.51221521.svg"
+  },
+  {
+    "revision": "c45fc81d1a0a8c16215c4795faea5b00",
+    "url": "/react_app_1/static/media/logo.c45fc81d.png"
+  },
+  {
+    "revision": "e7c19b81222234e0adc2e55fa29c68c8",
+    "url": "/react_app_1/static/media/logo.e7c19b81.png"
+  },
+  {
+    "revision": "c0fdc821c6406431d4d4754bcdf2aac6",
+    "url": "/react_app_1/static/media/runic_altno.c0fdc821.ttf"
+  },
+  {
+    "revision": "1019210aac85cafe740ed74d0dec96e5",
+    "url": "/react_app_1/static/media/tree.1019210a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_app_1/precache-manifest.64683bf294e2302ca974b9d6affb5df6.js"
+  "/react_app_1/precache-manifest.2e3a52a2a5a426525e5dae51a4cef671.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react_app_1/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_app_1/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
